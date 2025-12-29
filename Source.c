86a7bb6e5e836a7129617a73210c48b7f2bd9cf2
dtreznik <73201280+dtreznik@users.noreply.github.com>
2025-12-29 17:27:14 -0500

    Implement file encryption and decryption functionality
    The program reads a file and either encrypts or decrypts its content based on user input. It uses a hashed key for the encryption/decryption process.

---
+++ Source.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "util.h"
+
+int main(int argc, char * argv[]) {
+	assert(argc == 4, "Invalid arguments provided ([PATH] [KEY] [MODE (E or D)]");
+	char* fileName = argv[1];
+	FILE* filePointer = fopen(fileName, "r");
+	assert(filePointer != NULL, "could not open file");
+	
+	fseek(filePointer, 0, SEEK_END);
+	int fileLen = ftell(filePointer);
+	rewind(filePointer);
+
+	char* fileContent = (char*)malloc(fileLen * sizeof(char));
+	fread(fileContent, sizeof(char), fileLen, filePointer);
+
+	const char* key = argv[2];
+	const unsigned int hashedKey = hash(key, strlen(key));
+	
+	bool enc = (argv[3][0] == 'E');
+	if (enc) {
+		printf("Encrypting...");
+		for (int i = 0; i < fileLen; i++) {
+			int temp = hashedKey;
+			while (temp > 0) {
+				fileContent[i] ^= (((temp % 10) << 4) | (temp % 10));
+				temp = temp / 10;
+			}
+		}
+		freopen(fileName, "w", filePointer);
+		fwrite(fileContent, sizeof(char), fileLen, filePointer);
+		
+		printf("Done");
+	}
+	else {
+		printf("Decrypting...");
+		const unsigned int hashRev = reverseNum(hashedKey);
+		for (int i = 0; i < fileLen; i++) {
+			int temp = hashRev;
+			while (temp > 0) {
+				fileContent[i] ^= (((temp % 10) << 4) | (temp % 10));
+				temp = temp / 10;
+			}
+		}
+		freopen(fileName, "w", filePointer);
+		fwrite(fileContent, sizeof(char), fileLen, filePointer);
+		printf("Done");
+	}
+	fclose(filePointer);
+	
+	return 0;
+}
